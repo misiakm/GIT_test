0bdb3a4334ffcac2c7f72ea698caf0361ff22835
misiakm <35305579+misiakm@users.noreply.github.com>
2018-01-10 22:48:01 +0100

    Edycja Wiersza

--- testowy/testowy/Controllers/HomeController.cs
+++ testowy/testowy/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace testowy.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            return View("abc");
         }
     }
 }
